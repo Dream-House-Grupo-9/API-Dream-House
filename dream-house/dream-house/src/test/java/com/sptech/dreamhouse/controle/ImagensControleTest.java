2bf824efbc5564fd8eea4731dc660023a5ec1ad9
unknown <anderson.deitaqu4@gmail.com>
2022-05-29 22:11:08 -0300

    ajuste para deploy

---
+++ dream-house/dream-house/src/test/java/com/sptech/dreamhouse/controle/ImagensControleTest.java
@@ -0,0 +1,14 @@
+package com.sptech.dreamhouse.controle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImagensControleTest {
+
+    @Test
+    void inserirImagem() {
+
+
+    }
+}
